73c413ddad56218d51350e1ed7e74e74223fafd4
Marina Garcia <marina.garcia@qaracter.com>
2025-12-05 12:14:34 +0000

    Final: added test calls to verify it works

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Utility calc = new Utility();
+
+        // Prueba de Adición
+        System.out.println("Suma: " + calc.addThree(1, 2, 3));
+
+        // Prueba de Sustracción
+        System.out.println("Resta: " + calc.subtractThree(10, 5, 2));
+    }
+}

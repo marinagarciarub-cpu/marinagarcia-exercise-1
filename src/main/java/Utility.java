22dbe5fe0cf6f1176ee1c79d8193c8b653bdcdb5
Marina Garcia <marina.garcia@qaracter.com>
2025-12-05 11:48:23 +0000

    Feature: Subtraction (Initial)
    Implemented initial method to subtract one integer from another (a - b) and ensured addThree is present.

---
+++ src/main/java/Utility.java
@@ -0,0 +1,11 @@
+public class Utility {
+
+    public int addThree(int a, int b, int c) {
+        return a + b + c;
+    }
+
+
+    public int subtractTwo(int a, int b) {
+        return a - b;
+    }
+}
